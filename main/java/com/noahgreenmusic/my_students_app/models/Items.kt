c5f0116918237ada3ec668463f82135c221e9c40
noahgreenmusic <erabornoah@gmail.com>
2021-07-20 21:56:59 +0100

    Add files via upload

---
+++ main/java/com/noahgreenmusic/my_students_app/models/Items.kt
@@ -0,0 +1,7 @@
+package com.noahgreenmusic.my_students_app.models
+
+data class Items(
+    val id: Int,
+    val name: String,
+    val price: Int
+)
---
+++ main/java/com/noahgreenmusic/my_students_app/models/Student.kt
@@ -0,0 +1,10 @@
+package com.noahgreenmusic.my_students_app.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Student(
+    @SerializedName("Class")
+    var `class`: String,
+    var name: String,
+    var seat: String
+)
